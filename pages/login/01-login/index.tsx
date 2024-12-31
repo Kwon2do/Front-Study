88b89559a0b4b355a521d8397fe42f7dd20f7dc4
kwon <dohun_dev@naver.com>
2024-12-31 17:13:24 +0900

    액세스 토큰 recoil 저장 및 fetchPolicy 이해 #cache-first #network-only

--- pages/login/01-login/index.tsx
+++ pages/login/01-login/index.tsx
@@ -1,43 +1,72 @@
 import { JSX } from "react";
+import React from "react";
 import { useState } from "react";
+import { useRouter } from "next/router";
 import { gql, useMutation } from "@apollo/client";
-import { IMutationLoginUserArgs, IMutation } from "@/commons/types/generated/types";
+import { accessToken } from "@/commons/stores";
+import { useRecoilState } from "recoil";
+import type {
+  IMutationLoginUserArgs,
+  IMutation,
+} from "@/commons/types/generated/types";
 //query문 작성
 const LOGIN_USER = gql`
-    mutation loginUser($email: String!, $password: String!) {
-        loginUser(email: $email, password: $password) {
-            accessToken
-        }
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      accessToken
     }
-`
+  }
+`;
 export default function LoginPage(): JSX.Element {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    //useMutation<반환타입, 인자타입>
-    const [loginUser] = useMutation<Pick<IMutation, "loginUser">,IMutationLoginUserArgs>(LOGIN_USER);
-    const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setEmail(event.target.value);
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //useMutation<반환타입, 인자타입>
+  const [loginUser] = useMutation<
+    Pick<IMutation, "loginUser">,
+    IMutationLoginUserArgs
+  >(LOGIN_USER);
+  const [, setAccessToken] = useRecoilState(accessToken);
+
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setEmail(event.target.value);
+  };
+  const onChangePassword = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    setPassword(event.target.value);
+  };
+  const onClickLogin = async (): Promise<void> => {
+    try {
+      //1. 로그인 mutation 전송 후 accessToken 받아오기
+      const result = await loginUser({
+        variables: {
+          //short hand
+          email,
+          password,
+        },
+      });
+      const accessToken = result.data?.loginUser.accessToken;
+      console.log(accessToken);
+
+      //2. 받아온 accessToken recoil로 전달
+      if (accessToken === undefined) {
+        alert("로그인 실패!");
+        return;
+      }
+      setAccessToken(accessToken);
+
+      //3. 로그인 성공 페이지로 이동
+      router.push("/login/01-login-success");
+    } catch (error) {
+      if (error instanceof Error) alert(error.message);
     }
-    const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setPassword(event.target.value);
-    }
-    const onClickLogin = async(): Promise<void> => {
-        //1. 로그인 mutation 전송 후 accessToken 받아오기
-        const result = await loginUser({
-            variables: {
-                //short hand
-                email,
-                password
-            }
-        })
-        const accessToken = result.data?.loginUser.accessToken;
-        console.log(accessToken);
-    }
-    return (
-        <>
-            이메일: <input type="text" onChange={() => { }} />
-            비밀번호: <input type="password" onChange={() => { }} />
-            <button onClick={() => { }}>로그인</button>
-        </>
-    )
+  };
+  return (
+    <>
+      이메일: <input type="text" onChange={() => {}} />
+      비밀번호: <input type="password" onChange={() => {}} />
+      <button onClick={() => {}}>로그인</button>
+    </>
+  );
 }
---
+++ src/commons/stores/index.ts
@@ -0,0 +1,5 @@
+import { atom } from "recoil";
+export const accessToken = atom({
+    key:"accessToken",
+    default:""
+})
